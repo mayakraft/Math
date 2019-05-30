8097e35270fdfe7e8b1cf3f1cca012f378ef6a98
Robby Kraft <robbykraft@gmail.com>
2019-05-30 19:43:19 -0400

    no arrow functions in prototypes to prevent overwriting this

--- math.js
+++ math.js
@@ -1065,10 +1065,10 @@
   Matrix2.makeIdentity = () => Matrix2(1, 0, 0, 1, 0, 0);
   Matrix2.makeTranslation = (tx, ty) => Matrix2(1, 0, 0, 1, tx, ty);
   Matrix2.makeRotation = ((angle, origin) => Matrix2(
-    make_matrix2_rotation(angle, origin).map(n => clean_number(n, 13)),
+    make_matrix2_rotation(angle, origin).map(n => clean_number(n, 13))
   ));
   Matrix2.makeReflection = ((vector, origin) => Matrix2(
-    make_matrix2_reflection(vector, origin).map(n => clean_number(n, 13)),
+    make_matrix2_reflection(vector, origin).map(n => clean_number(n, 13))
   ));
 
   function Prototype (subtype, prototype) {
